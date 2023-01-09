467f5e2b314d597e87ab115f1deeef799be0b3b6
Roch Armengaud <armengaud.roch@live.fr>
2023-01-09 16:51:46 +0100

    Project done, preparing for exam

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -1,21 +1,25 @@
 import '../styles/features.css';
 
-export const Features = (props) => {
+import iconChat from '../assets/icon-chat.png';
+import iconMoney from '../assets/icon-money.png';
+import iconSecurity from '../assets/icon-security.png';
+
+export const Features = () => {
   return (
     <section className="features">
       <h2 className="sr-only">Features</h2>
       <div className="feature-item">
-        <img src={props.img1} alt="Chat Icon" className="feature-icon" />
+        <img src={iconChat} alt="Chat Icon" className="feature-icon" />
         <h3 className="feature-item-title">You are our #1 priority</h3>
         <p>Need to talk to a representative? You can get in touch through our 24/7 chat or through a phone call in less than 5 minutes.</p>
       </div>
       <div className="feature-item">
-        <img src={props.img2} alt="Chat Icon" className="feature-icon" />
+        <img src={iconMoney} alt="Chat Icon" className="feature-icon" />
         <h3 className="feature-item-title">More savings means higher rates</h3>
         <p>The more you save with us, the higher your interest rate will be!</p>
       </div>
       <div className="feature-item">
-        <img src={props.img3} alt="Chat Icon" className="feature-icon" />
+        <img src={iconSecurity} alt="Chat Icon" className="feature-icon" />
         <h3 className="feature-item-title">Security you can trust</h3>
         <p>We use top of the line encryption to make sure your data and money is always safe.</p>
       </div>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,17 +8,13 @@ import BankTree from '../assets/bank-tree.jpeg';
 import { Footer } from '../components/Footer';
 import { Features } from '../components/Features';
 
-import iconChat from '../assets/icon-chat.png';
-import iconMoney from '../assets/icon-money.png';
-import iconSecurity from '../assets/icon-security.png';
-
 export const Homepage = () => {
   return (
     <>
       <Navbar />
       <main>
         <Hero img={BankTree} />
-        <Features img1={iconChat} img2={iconMoney} img3={iconSecurity} />
+        <Features />
       </main>
       <Footer />
     </>
